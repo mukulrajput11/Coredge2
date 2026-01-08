7803bd4968ef7f67fa29dad0a786c7d6cc7d5191
LAKSHYA SINGLA <lakshyasingla125@gmail.com>
2026-01-08 17:48:06 +0530

    Footer Completed with changes(08/01/26)

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -7,16 +7,16 @@ const logos = [
 ]
 export default function Footer() {
     return (
-        <footer className="relative bg-black text-white overflow-hidden ">
+        <footer className="relative bg-[#0B0B0B] text-white overflow-hidden ">
             {/* Main Content */}
-            <div className="relative z-10 max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 pt-16 sm:pt-20 pb-14 sm:pb-16">
+            <div className="relative z-10 max-w-full mx-auto px-4 sm:px-6 lg:px-8 pt-16 sm:pt-20 pb-14 sm:pb-16">
 
                 {/* Top Grid */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-16">
 
                     {/* Platforms */}
                     <div>
-                        <h3 className="text-[32px] lg:text-[32px] md:text-[32px] xl:text-[36px] font-semibold mb-5 sm:mb-6 ">
+                        <h3 className=" lg:text-[32px] md:text-[32px] xl:text-[36px] 2xl:text-[56px] font-semibold mb-5 sm:mb-6 ">
                             Platforms
                         </h3>
                         <ul className="space-y-2 sm:space-y-3 text-auto font-normal font-inter  lg:text-[20px] text-[#fff] ">
@@ -32,7 +32,7 @@ export default function Footer() {
 
                     {/* Industry */}
                     <div>
-                        <h3 className="text-[32px] lg:text-[32px] md:text-[32px] xl:text-[36px] font-semibold mb-5 sm:mb-6">
+                        <h3 className="text-[34px] lg:text-[32px] md:text-[32px] xl:text-[36px] 2xl:text-[56px]  font-semibold mb-5 sm:mb-6">
                             Industry
                         </h3>
                         <ul className="space-y-2 sm:space-y-3 text-auto font-normal font-inter  lg:text-[20px] text-[#fff]">
@@ -47,7 +47,7 @@ export default function Footer() {
 
                     {/* Company */}
                     <div>
-                        <h3 className="text-[32px] lg:text-[32px] md:text-[32px] xl:text-[36px] font-semibold mb-5 sm:mb-6">
+                        <h3 className="text-[34px] lg:text-[32px] md:text-[32px] xl:text-[36px] 2xl:text-[56px] font-semibold  sm:mb-6">
                             Company
                         </h3>
                         <ul className="space-y-2 sm:space-y-3 text-auto font-normal font-inter  lg:text-[20px] text-[#fff]">
@@ -60,29 +60,46 @@ export default function Footer() {
 
                     {/* Get in Touch */}
                     <div>
-                        <h3 className="text-[32px] lg:text-[32px] md:text-[32px] xl:text-[36px] lg:w-[250px] font-semibold mb-5 sm:mb-6">
+                        <h3 className="text-[34px] lg:text-[32px] md:text-[32px] xl:text-[36px] 2xl:text-[56px]  font-semibold mb-5 sm:mb-6 w-full lg:w-[110%] 2xl:w-[120%]">
                             Get in Touch
                         </h3>
-                                    <div className="flex gap-3 sm:gap-4 w-[250px] ">
-                            {logos.map((logo,i) => (
-                               <div key={i} className="flex items-center justify-center w-[42px] h-[42px] pl-1">
-                                <img
-                                    src={logo}
-                                    alt={logo}
-                                    className="h-auto w-auto object-fill gap-[10px] cursor-pointer rounded-[8px]"
-                                />
-                               </div>
+                        <div className="flex gap-3 sm:gap-4 w-[250px] ">
+                            {logos.map((logo, i) => (
+                                <div key={i} className="flex items-center justify-center w-[42px] h-[42px] pl-1">
+                                    <img
+                                        src={logo}
+                                        alt={logo}
+                                        className="h-auto w-auto object-fill gap-[10px] cursor-pointer rounded-[8px]"
+                                    />
+                                </div>
                             ))}
                         </div>
                     </div>
                 </div>
+                {/* Background Watermark */}
+                <div className="relative text-center select-none pointer-events-none w-full mt-10">
+                    <img
+                        src="Footer-logo.png"
+                        alt="Coredge Watermark"
+                        className="
+                        mx-auto
+                        w-[100%]
+                        sm:w-[100%]
+                        md:w-[750px]
+                        lg:w-[100%]
+                        xl:w-[100%]
+                        2xl:w-[100%]
+                        max-w-full
+                         xl:h-[50vh] 2xl:h-[65vh] lg:h-[35vh] md:h-[20vh] sm:h-[100vh] w-full object-fill "
+                    />
+                </div>
 
                 {/* Bottom Row */}
-                <div className="mt-12 flex flex-col lg:flex-row justify-between items-center gap-6 text-auto text-[#fff]">
+                <div className="mt-12 flex flex-col lg:flex-row xl:flex-row 2xl:flex-row md:flex-row justify-between items-center gap-6 text-auto text-[#fff] ">
 
 
 
-                    <div className="flex gap-6 sm:gap-20 md:gap-[100px]">
+                    <div className="flex flex-col gap-10 md:gap-16 lg:flex-row xl:flex-row 2xl:flex-row md:flex-row sm:flex-col">
                         <p className="text-center lg:text-left font-medium  sm:text-[16px] md:text-[16px] lg:text-[20px] xl:text-[20px] ">
                             © 2025 Coredge. All Rights Reserved
                         </p>
@@ -98,23 +115,7 @@ export default function Footer() {
                 </div>
             </div>
 
-            {/* Background Watermark */}
-            <div className="relative pb-6 sm:pb-8 lg:pb-10 text-center select-none pointer-events-none w-full">
-                <img
-                    src="Footer-logo.png"
-                    alt="Coredge Watermark"
-                    className="
-            mx-auto
-            w-[100%]
-            sm:w-[100%]
-            md:w-[750px]
-            lg:w-[1000px]
-            xl:w-[90%]
 
-            max-w-[1440px]
-            object-contain"
-                />
-            </div>
             {/* <div className="relative pb-6 sm:pb-8 lg:pb-10 text-center select-none pointer-events-none w-full">
   <img
     src="Footer-logo.png"
